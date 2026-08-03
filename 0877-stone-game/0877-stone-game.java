75b2672d660d5ed5fbaeaa70b704179dd90af57a
Swattik Shit <shitswattik@gmail.com>
2026-08-03 22:49:08 +0530

    Time: 10 ms (11.71%), Space: 57.8 MB (5.09%) - LeetHub

--- 0877-stone-game/0877-stone-game.java
+++ 0877-stone-game/0877-stone-game.java
@@ -1,5 +1,35 @@
+// Optimal Solution
+
+// class Solution {
+//     public boolean stoneGame(int[] piles) {
+//         return true;
+//     }
+// }
+
+
 class Solution {
+    Integer[][] dp;
     public boolean stoneGame(int[] piles) {
-        return true;
+        int n = piles.length;
+        dp = new Integer[n][n];
+
+        return solve(piles , 0 , n -1) > 0;
+
+    }
+
+    private int solve(int[] piles , int left , int right){
+        if(left == right ){
+            return piles[left];
+        }
+        if(dp[left][right] != null ){
+            return dp[left][right];
+        }
+
+        int leftTake = Math.min(left , solve(piles , left , right -1));
+        int rightTake = Math.min(right , solve(piles , left + 1 , right));
+
+        return dp[left][right] = Math.max(leftTake,rightTake);
     }
 }
+
+
